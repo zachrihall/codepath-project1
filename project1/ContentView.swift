5d4d2eb35df0f9bd8a37ab694da9de0cab971106
zachrihall <hallzachri@gmail.com>
2026-09-15 13:43:46 -0400

    Initial Commit

---
+++ project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project1
+//
+//  Created by Zach on 9/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Zach on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
